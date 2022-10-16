608bd35ebf3f8d33cf508726e84449af3918374a
Vishalrai@24 <97247801+v900rai@users.noreply.github.com>
2022-10-16 16:19:57 +0530

    Add files via upload

---
+++ cube.cpp
@@ -0,0 +1,12 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+    int x=2;
+    int cube;
+    cout<<"number is"<<x<<endl; // vishal rai
+    cout<<"cube of the number is"<<x*x*x<<endl;
+    return 0;
+    
+
+}
