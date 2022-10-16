e28d2b922a12ac1b74bf7ac1551311ac8ed0dd46
Vishalrai@24 <97247801+v900rai@users.noreply.github.com>
2022-10-16 16:14:03 +0530

    Add files via upload

---
+++ rectangle.cpp
@@ -0,0 +1,14 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+    int length=10;
+    int breadth=20;
+    cout<<"length"<<length<<endl;// vishal Rai
+    cout<<"breadth"<<breadth<<endl;
+    int area=length*breadth;
+    cout<<"Area is   "<<area<<endl;
+    return 0;
+
+    
+} 
