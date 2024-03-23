f9a384a8dfec9d7daa1d6b135559ffef0f1ec58b
Vishalrai@24 <97247801+v900rai@users.noreply.github.com>
2024-03-23 21:25:46 +0530

    Add files via upload

---
+++ student.java
@@ -0,0 +1,22 @@
+package studendInsert;
+
+public class student
+{
+	
+		String name;
+		double marks;
+		public student(String name, double marks )
+		{
+			this.name=name;
+			this.marks=marks;
+		}
+		@Override
+		public String toString() {
+			return "student [name=" + name + ", marks=" + marks + "]";
+		
+		
+		
+
+	}
+
+}
